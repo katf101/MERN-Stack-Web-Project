b193fda3db79f17e752f5962cc6d829008c159af
katf101 <vawav11@gmail.com>
2022-12-11 22:50:37 +0900

    feat: 미들웨어 설정

--- backend/src/setupServer.ts
+++ backend/src/setupServer.ts
@@ -7,7 +7,13 @@ import {
   NextFunction,
 } from "express";
 import http from "http";
-
+import cors from "cors";
+import helmet from "helmet";
+import hpp from "hpp";
+import compression from "compression";
+import cookierSession from "cookie-session";
+import HTTP_STATUS from "http-status-codes";
+import "express-async-errors";
 export class MyServer {
   private app: Application;
 
@@ -23,9 +29,33 @@ export class MyServer {
     this.startServer(this.app);
   }
 
-  private securityMiddleware(app: Application): void {}
+  private securityMiddleware(app: Application): void {
+    app.use(
+      cookierSession({
+        name: "session",
+        keys: ["test1", "test2"],
+        maxAge: 24 * 7 * 3600000, // 쿠키의 최대 기간 7일로 설정
+        secure: false,
+      })
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(
+      cors({
+        // origin: "https://dev.chatapp.com",
+        origin: "*",
+        credentials: true,
+        optionsSuccessStatus: 200,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      })
+    );
+  }
 
-  private standardMiddleware(app: Application): void {}
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: "50mb" })); // 요청마다 50mb를 넘기면 오류 발생
+    app.use(urlencoded({ extended: true, limit: "50mb" }));
+  }
 
   private routerMiddleware(app: Application): void {}
 
