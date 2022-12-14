c22a9d08220ad9373097c28d5fe97713bf41fb20
katf101 <vawav11@gmail.com>
2022-12-14 23:41:11 +0900

    feat: config 셋팅

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,13 +2,20 @@ import express, { Express } from "express";
 
 import { MyChatServer } from "./setupServer";
 import databaseConnection from "./setupDatabase";
+import { config } from "./config";
 
 class Application {
   public initialize(): void {
+    // 앱 시작 시 loadConfig 호출 되고 데이터베이스 시작 -> Server 실행
+    this.loadConfig();
     databaseConnection();
     const app: Express = express();
     const server: MyChatServer = new MyChatServer(app);
     server.start();
+  }
+
+  private loadConfig(): void {
+    config.validateConfig();
   }
 }
 
