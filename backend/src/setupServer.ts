4fedd20a1d250cf1083a405d60f72d44159efe43
katf101 <vawav11@gmail.com>
2022-12-19 23:38:13 +0900

    feat: global error handler

--- backend/src/setupServer.ts
+++ backend/src/setupServer.ts
@@ -21,6 +21,10 @@ import appliationRoutes from "./routes";
 
 import "express-async-errors";
 import { config } from "./config";
+import {
+  CustomError,
+  IErrorREsponse,
+} from "./shared/globals/helpers/error-handler";
 
 const SERVER_PORT = 5000;
 
@@ -71,7 +75,28 @@ export class MyChatServer {
     appliationRoutes(app);
   }
 
-  private globalErrorHandler(app: Application): void {}
+  private globalErrorHandler(app: Application): void {
+    app.all("*", (req: Request, res: Response) => {
+      res
+        .status(HTTP_STATUS.NOT_FOUND)
+        .json({ message: `${req.originalUrl} not found` });
+    });
+
+    app.use(
+      (
+        error: IErrorREsponse,
+        req: Request,
+        res: Response,
+        next: NextFunction
+      ) => {
+        console.log(error);
+        if (error instanceof CustomError) {
+          return res.status(error.statusCode).json(error.serializeErrors());
+        }
+        next();
+      }
+    );
+  }
 
   // 인스턴스 전달
   private async startServer(app: Application): Promise<void> {
